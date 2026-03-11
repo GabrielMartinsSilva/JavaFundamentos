9b70e058997bf97630f430b12b09bbc587125b80
Gabriel Martins <gamartins77@gmail.com>
2026-03-11 20:43:36 -0300

    Aula dados não primitivos

---
+++ src/TiposDeDados/DadosNaoPrimitivos.java
@@ -0,0 +1,14 @@
+package TiposDeDados;
+
+public class DadosNaoPrimitivos {
+    public static void main(String[] args) {
+
+        /*Dados não primitivos
+        * String, array, class, enum*/
+
+        String nome = "Gabriel";
+        nome.toUpperCase();
+        System.out.println(nome);
+        
+    }
+}

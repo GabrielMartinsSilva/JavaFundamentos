f1ec51459cf11f58091c5bd54d516655d99ceff5
Gabriel Martins <gamartins77@gmail.com>
2026-03-11 13:06:56 -0300

    Dados primitivos

---
+++ src/TiposDeDados/DadosPrimitivos.java
@@ -0,0 +1,19 @@
+package TiposDeDados;
+
+public class DadosPrimitivos {
+    public static void main(String[] args) {
+
+        /*Dados Primitivos
+        * int, double, float, char, boolean, short
+        */
+
+        int idade = 16;
+        double altura = 1.65;
+        char incial = 'n';
+        boolean vivoOumorto = true;
+
+        System.out.println(idade);
+
+
+    }
+}

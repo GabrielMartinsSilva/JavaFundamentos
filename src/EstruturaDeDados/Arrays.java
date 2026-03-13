9beaf38177e4a45a45ac4af3f9e0e09d2eae3c69
Gabriel Martins <gamartins77@gmail.com>
2026-03-13 15:40:02 -0300

    Aula de array

---
+++ src/EstruturaDeDados/Arrays.java
@@ -0,0 +1,17 @@
+package EstruturaDeDados;
+
+public class Arrays {
+
+    public static void main(String[] args) {
+        String [] nome = new String[4];
+        nome[0] = "Gabriel";
+
+        int [] idade = new int [4];
+        System.out.println(idade[0]);
+
+        double [] decimal = new double[2];
+        System.out.println(decimal[1]);
+
+
+    }
+}
